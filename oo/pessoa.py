748fad5872c6f35ae130d382f37be07d201e2e23
Anthony Silva <hedreson@icloud.com>
2022-03-01 01:32:20 -0300

    Criado e removido atributo dinâmico do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -13,13 +13,14 @@ class Pessoa:
 
 if __name__ == '__main__':
 
-    print(Pessoa.cumprimentar(usuario))
-    print(id(usuario))
-    print(usuario.cumprimentar())
 
     usuario= Pessoa(nome='Bam')
     #usuario.nome = 'Liane'
     print(f'Nome: {usuario.nome} - Idade: {usuario.idade} - Peso: {usuario.peso}')
+
+    print(Pessoa.cumprimentar(usuario))
+    print(id(usuario))
+    print(usuario.cumprimentar())
 
     amanda = Pessoa(nome='Amanda', idade=32, peso=67.52)
     luciano = Pessoa(amanda, nome='Luciano')
@@ -27,5 +28,11 @@ if __name__ == '__main__':
     for filho in luciano.filhos:
         print(filho.nome)
 
+    luciano.sobrenome = 'Soares'
 
+    print(luciano.__dict__)
+    del luciano.filhos
+    print(luciano.__dict__)
+
+    print(amanda.__dict__)
 
