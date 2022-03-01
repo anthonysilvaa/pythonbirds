714e437df938982497e04a30471332d5ee62a5d5
Anthony Silva <hedreson@icloud.com>
2022-03-01 00:47:16 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+
+    def cumprimentar(self):
+
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+
+    usuario = Pessoa()
+    print(Pessoa.cumprimentar(usuario))
+    print(id(usuario))
+    print(usuario.cumprimentar())
