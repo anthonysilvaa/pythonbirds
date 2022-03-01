128b191128247caa8a31354f9200abd14d0794d6
Anthony Silva <hedreson@icloud.com>
2022-03-01 01:00:45 -0300

    Criado atributos de instância nome, idade e peso

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,10 @@
 class Pessoa:
 
+    def __init__(self, nome: str, idade: int, peso: float):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+
     def cumprimentar(self):
 
         return f'Olá {id(self)}'
@@ -7,7 +12,12 @@ class Pessoa:
 
 if __name__ == '__main__':
 
-    usuario = Pessoa()
+    usuario = Pessoa('Amanda', 32, 67.52)
+
+    usuario.nome = 'Liane'
+
+    print(f'Nome: {usuario.nome} - Idade: {usuario.idade} - Peso: {usuario.peso}')
+
     print(Pessoa.cumprimentar(usuario))
     print(id(usuario))
     print(usuario.cumprimentar())
